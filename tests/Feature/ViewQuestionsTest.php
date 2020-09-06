c913a3a4e3034239c27c7cff39255fee4e2fbba3
Carlos Xu <srj520w@gmail.com>
2020-09-06 22:53:05 +0800

    refactory

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -26,7 +26,7 @@ class ViewQuestionsTest extends TestCase
     public function user_can_view_a_single_question()
     {
         // 创建一个问题
-        $question = factory(Question::class)->create();
+        $question = factory(Question::class)->create(['published_at' => Carbon::parse('-1 week')]);
         // 访问链接
         $test = $this->get('/questions/' . $question->id);
         // 正常返回200
