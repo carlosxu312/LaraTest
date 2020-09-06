ee720ad88944885f2dfd83fd3bf23e78bf6f01eb
Carlos Xu <srj520w@gmail.com>
2020-09-06 22:20:38 +0800

    test view single question

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Question;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ViewQuestionsTest extends TestCase
 {
+    use RefreshDatabase;
+
    /** @test */
     public function user_can_view_questions()
     {
@@ -16,5 +19,18 @@ class ViewQuestionsTest extends TestCase
         $test = $this->get('/questions');
         // 正常返回
         $test->assertStatus(200);
+    }
+
+    /** @test */
+    public function user_can_view_a_single_question()
+    {
+        // 创建一个问题
+        $question = factory(Question::class)->create();
+        // 访问链接
+        $test = $this->get('/questions/' . $question->id);
+        // 正常返回200
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
     }
 }
